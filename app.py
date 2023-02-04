2514fead89a7b40cb9e1a2c57cdad4fc5598ad07
NekoSarada1101 <ryoyukikazu11@gmail.com>
2023-02-04 18:08:20 +0900

    GCPの料金通知

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ import logging
 import json
 import random
 import openai
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
-from google.cloud import firestore, secretmanager
+from google.cloud import firestore, secretmanager, bigquery
 
 
 # constant ============================================
 secret_client = secretmanager.SecretManagerServiceClient()
 firestore_client = firestore.Client()
+bq_client = bigquery.Client()
 
 SLACK_BOT_TOKEN = secret_client.access_secret_version(request={'name': 'projects/831232013080/secrets/SECRETARY_BOT_V2_SLACK_BOT_TOKEN/versions/latest'}).payload.data.decode('UTF-8')
 SLACK_SIGNING_SECRET = secret_client.access_secret_version(request={'name': 'projects/831232013080/secrets/SECRETARY_BOT_V2_SLACK_SIGNING_SECRET/versions/latest'}).payload.data.decode('UTF-8')
@@ -22,6 +23,8 @@ TWITCH_CLIENT_ID = secret_client.access_secret_version(request={'name': 'project
 TWITCH_CLIENT_SECRET = secret_client.access_secret_version(request={'name': 'projects/831232013080/secrets/SECRETARY_BOT_V2_TWITCH_CLIENT_SECRET/versions/latest'}).payload.data.decode('UTF-8')
 USER_ID = os.environ.get('USER_ID')
 TWITCH_SLACK_CHANNEL_ID = os.environ.get('TWITCH_SLACK_CHANNEL_ID')
+GCP_NOTICE_SLACK_CHANNEL_ID = os.environ.get('GCP_NOTICE_SLACK_CHANNEL_ID')
+TABLE_ID = os.environ.get('TABLE_ID')
 
 flask_app = Flask(__name__)
 bolt_app = App(token=SLACK_BOT_TOKEN,
@@ -606,7 +609,92 @@ def response_message(event, ack, say):
         logger.info('===== END text-davinci-003 mention response =====')
 
 
-@bolt_app.command('/openai')
+@ flask_app.route('/gcp/notify_gcp_cost', methods=['POST'])
+def notify_gcp_cost():
+    logger.info('===== START notify gcp cost =====')
+    logger.info('request={}'.format(request.get_data()))
+
+    try:
+        logger.info('----- get bigquery gcp cost top 3 -----')
+        yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+        top_query = """
+            SELECT service.description as service, sku.description as sku, SUM(cost) AS cost
+            FROM `{}`
+            WHERE project.id = 'slackbot-288310' AND FORMAT_DATE('%Y-%m-%d',usage_start_time) = '{}'
+            GROUP BY service, sku
+            HAVING cost != 0
+            ORDER BY cost desc
+            LIMIT 3
+        """.format(TABLE_ID, yesterday)
+        logger.info('top_query={}'.format(top_query))
+
+        query_job = bq_client.query(top_query)
+        top_query_result = []
+        for row in query_job:
+            top_query_result.append({'service': row['service'], 'sku': row['sku'], 'cost': row['cost']})
+        logger.info('top_query_result={}'.format(top_query_result))
+
+        logger.info('----- get bigquery gcp cost total -----')
+        total_query = """
+            SELECT SUM(cost) AS cost
+            FROM `{}`
+            WHERE project.id = 'slackbot-288310' AND FORMAT_DATE('%Y-%m-%d',usage_start_time) = '{}'
+        """.format(TABLE_ID, yesterday)
+        logger.info('top_query={}'.format(total_query))
+
+        query_job = bq_client.query(total_query)
+        total_query_result = None
+        for row in query_job:
+            total_query_result = row['cost']
+        logger.info('total_query_result={}'.format(total_query_result))
+
+        logger.info('----- POST slack api send chat message -----')
+        color = '#'+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+        attachment = [{
+            'color': color,
+            'blocks': [
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text': '*{} Total Cost*\n{} JPY'.format(yesterday, total_query_result)
+                    }
+                },
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text':
+                        f'*{yesterday} Cost Top3*\n*{top_query_result[0]["service"]} : {top_query_result[0]["sku"]}*\n{top_query_result[0]["cost"]} JPY\n\n*{top_query_result[1]["service"]} : {top_query_result[1]["sku"]}*\n{top_query_result[1]["cost"]} JPY\n\n*{top_query_result[2]["service"]} : {top_query_result[2]["sku"]}*\n{top_query_result[2]["cost"]} JPY'
+                    }
+                }
+            ]
+        }]
+
+        payload = {
+            'token': SLACK_BOT_TOKEN,
+            'channel': GCP_NOTICE_SLACK_CHANNEL_ID,
+            'text': 'Notify Today GCP Cost',
+            'attachments': json.dumps(attachment),
+            'username': 'Notify GCP Cost',
+            'icon_emoji': ':gcp:',
+        }
+        logger.info('payload={}'.format(payload))
+
+        response = requests.post('https://slack.com/api/chat.postMessage', data=payload)
+        logger.info('response={}'.format(response.text))
+
+        return 'notify gcp cost success!', 204
+
+    except Exception as e:
+        logger.error(e)
+    finally:
+        logger.info('===== END notify gcp cost =====')
+
+
+@ bolt_app.command('/openai')
 def test(ack, say, command):
     ack()
     logger.info('===== START slash command /openai =====')
@@ -626,25 +714,25 @@ def test(ack, say, command):
         logger.info('===== END slash command /openai =====')
 
 
-@bolt_app.command('/test')
+@ bolt_app.command('/test')
 def test(ack, say, command):
     ack()
     logger.info('request={}'.format(command))
     say('OK!')
 
 
-@bolt_app.event("message")  # ロギング
+@ bolt_app.event("message")  # ロギング
 def handle_message_events(body, logger):
     logger.info(body)
 
 
-@flask_app.route('/', methods=['POST'])
+@ flask_app.route('/', methods=['POST'])
 def index():
     logger.info('request={}'.format(request.get_data()))
     return 'Flask Test'
 
 
-@flask_app.route('/slack/events', methods=['POST'])
+@ flask_app.route('/slack/events', methods=['POST'])
 def slack_events():
     return handler.handle(request)
 
